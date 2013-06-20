1f44d01a74c4db5ed69f31a5c7cd0d417f03af83
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2013-06-20 17:55:20 -0400

    Subiendo documentacion y archivo de prueba.

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ distribuidos
 ============
 
 Repositorio para archivos de Ayundatia de Sistemas Distribuidos UDP
+Uso:
+
+Para obtener la lista de usuarios se utiliza la siguiente url:
+
+http://localhost:8080/users GET
+
+Lo cual nos entrega todos los usuarios que están registrados.
+
+En el caso de que se quiera obtener los datos de un usuario en especifico se utiliza la siguiente url:
+
+http://localhost:8080/users/id_usuario GET
+
+donde el id_usuario es el id del usuario que se quiere obtener la información.
+
+Luego para agragar un nuevo usuario al sistema se realiza con la siguiente url:
+
+http://localhost:8080/users/new POST
+
+y como paramétros recibe:
+
+-> first_name
+-> last_name
+
+
---
+++ rest_in_python.py
@@ -0,0 +1,47 @@
+import json
+from bottle import route, run, request, abort, response
+
+users = {
+    '0': {
+        'first_name': 'nombre0',
+        'last_name': 'apellido0'
+    },
+    '1':{
+        'first_name':'nombre1',
+        'last_name':'apellido1'
+    }
+} 
+@route('/users', method='GET')
+def getUsers():
+    try:
+        response.content_type = 'text/json; charset=utf-8'
+        return json.dumps({ 'data' : users }, indent=4, separators=(',', ': '))
+    except:
+        abort(404, 'Error en parametros, falto .json')
+
+@route('/users/:id', method='GET')
+def getUser(id):
+    try:
+        entity = id.split(".")
+        if entity[1] == "json":
+                response.content_type = 'text/json; charset=utf-8'
+                return json.dumps({'data' : users[entity[0]]}, indent=4, separators=(',', ': '))
+        if entity[1] == "":
+                abort(404, 'No username with id %s' % id)
+        return entity
+    except:
+        abort(404, 'Error en parametros, falto .json')
+
+@route('/users/new', method='POST')
+def setUser():
+        first_name = request.forms.get('first_name')
+        last_name  = request.forms.get('last_name')
+	users[str(len(users))] = {
+		'first_name': first_name,
+		'last_name' : last_name
+	}
+        return json.dumps({'data':'usuario ingresado\n'}, indent=4, separators=(',', ': '))
+
+ 
+run(host='localhost', port=8080, debug=True)
+
